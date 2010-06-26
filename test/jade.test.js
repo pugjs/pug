cf3b37bea64506d62df5998b3da4d6d1e0957939
Tj Holowaychuk <tj@vision-media.ca>
2010-06-26 12:56:50 -0700

    Started tests

--- test/jade.test.js
+++ test/jade.test.js
@@ -5,8 +5,42 @@
 
 var jade = require('jade');
 
+// Shortcut
+
+var render = jade.render;
+
 module.exports = {
     'version': function(assert){
         assert.ok(/^\d+\.\d+\.\d+$/.test(jade.version), "Invalid version format");
+    },
+    
+    'test tags': function(assert){
+        var str = [
+            'p',
+            'div',
+            'img'
+        ].join('\n');
+
+        var html = [
+            '<p></p>',
+            '<div></div>',
+            '<img />'
+        ].join('');
+
+        assert.equal(html, render(str));
+    },
+    
+    'test attrs': function(assert){
+        assert.equal('<img src="&lt;script&gt;" />', render('img(src="<script>")'));
+        
+        assert.equal('<img src="/foo.png" />', render('img(src="/foo.png")'));
+        assert.equal('<img src="/foo.png" />', render('img(src  =  "/foo.png")'));
+        assert.equal('<img src="/foo.png" />', render('img(src:"/foo.png")'));
+        assert.equal('<img src="/foo.png" />', render('img(src  :  "/foo.png")'));
+        
+        assert.equal('<img src="/foo.png" alt="just some foo" />', render('img(src: "/foo.png", alt: "just some foo")'));
+        assert.equal('<img src="/foo.png" alt="just some foo" />', render('img(src   : "/foo.png", alt  :  "just some foo")'));
+        assert.equal('<img src="/foo.png" alt="just some foo" />', render('img(src="/foo.png", alt="just some foo")'));
+        assert.equal('<img src="/foo.png" alt="just some foo" />', render('img(src = "/foo.png", alt = "just some foo")'));
     }
 };
