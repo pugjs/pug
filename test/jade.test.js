f95b4948911bd24b67dba7145040a5f8a5f1503f
Tj Holowaychuk <tj@vision-media.ca>
2010-06-26 19:29:04 -0700

    Output generated js for "debug" option before compiling the function

--- test/jade.test.js
+++ test/jade.test.js
@@ -43,7 +43,7 @@ module.exports = {
         assert.equal('<div class="bar" id="foo"></div>', render('div(class="bar")#foo'));
         assert.equal('<div id="bar" class="foo"></div>', render('div(id="bar").foo'));
         
-        // assert.equal('<div class="foo bar baz"></div>', render('div.foo.bar.baz'));
+        assert.equal('<div class="foo bar baz"></div>', render('div.foo.bar.baz', { debug: true }));
         // assert.equal('<div class="foo bar baz"></div>', render('div(class="foo").bar.baz'));
         // assert.equal('<div class="foo bar baz"></div>', render('div.foo(class="bar").baz'));
         // assert.equal('<div class="foo bar baz"></div>', render('div.foo.bar(class="baz")'));
