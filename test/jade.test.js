9a77af6cde50a70bf8e351c3e314ed4d505e6d1b
Tj Holowaychuk <tj@vision-media.ca>
2010-06-27 11:34:05 -0700

    Added a test for html 5 mode

--- test/jade.test.js
+++ test/jade.test.js
@@ -272,6 +272,10 @@ module.exports = {
             err.message);
     },
     
+    'test html 5 mode': function(assert){
+        assert.equal('<!DOCTYPE html>\n<input type="checked" checked>', render('!!! 5\ninput(type="checkbox", checked)'));
+    },
+    
     'test attrs': function(assert){
         assert.equal('<img src="&lt;script&gt;" />', render('img(src="<script>")'), 'Test attr escaping');
         
