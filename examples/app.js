e3e86b6c3d3f8ceb585db8ca833146f1960da0fb
Tj Holowaychuk <tj@vision-media.ca>
2010-06-29 16:39:29 -0700

    No debug in example

--- examples/app.js
+++ examples/app.js
@@ -6,7 +6,7 @@
 var sys = require('sys'),
     jade = require('./../lib/jade');
 
-jade.renderFile(__dirname + '/layout.jade', { debug: true }, function(err, html){
+jade.renderFile(__dirname + '/layout.jade', function(err, html){
     if (err) throw err;
     sys.puts(html);
 });
