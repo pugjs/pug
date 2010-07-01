c996382021cf3702799f856866694746ed417fd6
Tj Holowaychuk <tj@vision-media.ca>
2010-06-30 21:01:05 -0700

    Added users example

--- examples/run.js
+++ examples/run.js
@@ -8,5 +8,15 @@ var sys = require('sys'),
 
 jade.renderFile(__dirname + '/layout.jade', function(err, html){
     if (err) throw err;
-    sys.puts(html);
+    sys.puts('\x1b[1mlayout.jade:\x1b[0m', html, '');
+});
+
+var users = [
+    { name: 'tj', roles: ['admin'] },
+    { name: 'simon', roles: [] },
+    { name: 'tobi', roles: ['manager'] }
+];
+jade.renderFile(__dirname + '/users.jade', { locals: { users: users }}, function(err, html){
+    if (err) throw err;
+    sys.puts('\x1b[1musers.jade:\x1b[0m', html, '');
 });
