4bdb48b318a3ee863ea2cfd620da79cda1f77e27
Tj Holowaychuk <tj@vision-media.ca>
2010-10-12 10:56:28 -0700

    Added failing test for leading text whitespace

--- test/jade.test.js
+++ test/jade.test.js
@@ -298,6 +298,8 @@ module.exports = {
     
     'test tag text': function(assert){
         assert.equal('some random text\n', render('| some random text'));
+        assert.equal('  some random text\n', render('|   some random text'));
+        assert.equal('  some random text  \n', render('|   some random text  '));
         assert.equal('<p>some random text</p>', render('p some random text'));
         assert.equal('foo\n<em>bar\n\n</em>baz\n', render('| foo\nem bar\n| baz'));
         // assert.equal('<p>(parens)</p>', render('p (parens)'));
