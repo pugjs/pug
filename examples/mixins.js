62b3ed6900086299e3f4a5c96544cdb10c1600c9
Tj Holowaychuk <tj@vision-media.ca>
2011-07-13 13:38:48 -0700

    better mixins example

--- examples/mixins.js
+++ examples/mixins.js
@@ -5,7 +5,14 @@
 
 var jade = require('./../lib/jade');
 
-jade.renderFile(__dirname + '/mixins.jade', function(err, html){
+var user = {
+    name: 'tj'
+  , pets: ['tobi', 'loki', 'jane', 'manny']
+};
+
+var options = { locals: { user: user }};
+
+jade.renderFile(__dirname + '/mixins.jade', options, function(err, html){
   if (err) throw err;
   console.log(html);
 });
