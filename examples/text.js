a37305e935dd9f6634e8fb00c54bd91a32217834
Tj Holowaychuk <tj@vision-media.ca>
2011-09-26 10:10:53 -0700

    Fixed text example

--- examples/text.js
+++ examples/text.js
@@ -3,8 +3,9 @@
  * Module dependencies.
  */
 
-var jade = require('./../lib/jade')
-  , str = require('fs').readFileSync(__dirname + '/text.jade', 'utf8')
-  , fn = jade.compile(str);
+var jade = require('./../')
+  , path = __dirname + '/text.jade'
+  , str = require('fs').readFileSync(path, 'utf8')
+  , fn = jade.compile(str, { filename: path, pretty: true });
 
 console.log(fn({ name: 'tj', email: 'tj@vision-media.ca' }));
