010834448f45bcb1d5733b65de47666812260b32
Tj Holowaychuk <tj@vision-media.ca>
2011-09-26 10:09:58 -0700

    Fixed rss example

--- examples/rss.js
+++ examples/rss.js
@@ -3,9 +3,15 @@
  * Module dependencies.
  */
 
-var jade = require('./../lib/jade');
+var jade = require('./../')
+  , path = __dirname + '/rss.jade'
+  , str = require('fs').readFileSync(path, 'utf8')
+  , fn = jade.compile(str, { filename: path, pretty: true });
 
-jade.renderFile(__dirname + '/rss.jade', function(err, xml){
-  if (err) throw err;
-  console.log(xml);
-});
+var items = [];
+
+items.push({ title: 'Example', description: 'Something', link: 'http://google.com' });
+items.push({ title: 'LearnBoost', description: 'Cool', link: 'http://learnboost.com' });
+items.push({ title: 'Express', description: 'Cool', link: 'http://expressjs.com' });
+
+console.log(fn({ items: items }));
