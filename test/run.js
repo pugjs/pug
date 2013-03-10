36e55af65ddd32cadaa9ca1792cac673a6242c7d
Stuart P. Bentley <stuart@testtrack4.com>
2013-03-10 17:04:52 -0400

    Some parts of error handling testing

--- test/run.js
+++ test/run.js
@@ -8,20 +8,45 @@ var jade = require('../')
 
 // test cases
 
-var cases = fs.readdirSync('test/cases').filter(function(file){
-  return ~file.indexOf('.jade');
-}).map(function(file){
-  return file.replace('.jade', '');
-});
+describe("compiled templates", function(){
+  var cases = fs.readdirSync('test/cases').filter(function(file){
+    return ~file.indexOf('.jade');
+  }).map(function(file){
+    return file.replace('.jade', '');
+  });
 
-cases.forEach(function(test){
-  var name = test.replace(/[-.]/g, ' ');
-  it(name, function(){
-    var path = 'test/cases/' + test + '.jade';
-    var str = fs.readFileSync(path, 'utf8');
-    var html = fs.readFileSync('test/cases/' + test + '.html', 'utf8').trim().replace(/\r/g, '');
-    var fn = jade.compile(str, { filename: path, pretty: true });
-    var actual = fn({ title: 'Jade' });
-    actual.trim().should.equal(html);
-  })
-});
+  cases.forEach(function(test){
+    var name = test.replace(/[-.]/g, ' ');
+    it(name, function(){
+      var path = 'test/cases/' + test + '.jade';
+      var str = fs.readFileSync(path, 'utf8');
+      var html = fs.readFileSync('test/cases/' + test + '.html', 'utf8').trim().replace(/\r/g, '');
+      var fn = jade.compile(str, { filename: path, pretty: true });
+      var actual = fn({ title: 'Jade' });
+      actual.trim().should.equal(html);
+    })
+  });
+})
+
+describe("error handling", function(){
+  var cases = fs.readdirSync('test/errors').filter(function(file){
+    return ~file.indexOf('.jade');
+  }).map(function(file){
+    return file.replace('.jade', '');
+  });
+
+  cases.forEach(function(test){
+    var name = test.replace(/[-.]/g, ' ');
+    it(name, function(done){
+      var path = 'test/errors/' + test + '.jade';
+      var str = fs.readFileSync(path, 'utf8');
+      var expected = fs.readFileSync('test/cases/' + test + '.html', 'utf8').trim().replace(/\r/g, '');
+      var fn = jade.compile(str, { filename: path, pretty: true, debug: true });
+      try{
+        fn({ title: 'Jade' });
+      } catch(err){
+        err.trim().should.equal(expected);
+      }
+    });
+  });
+})
