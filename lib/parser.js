pugjs/pug

commit 5a62e630774e6639824068db3ad889517737f4bc

Added pipe-less text support with immediate ".". Closes #157

lib/parser.js
@@ -359,6 +359,12 @@ Parser.prototype = {
         }
       }
 
+    // check immediate '.'
+    if ('.' == this.peek.val) {
+      tag.textOnly = true;
+      this.advance;
+    }
+
     // (text | code | ':')?
     switch (this.peek.type) {
       case 'text':
@@ -383,7 +389,7 @@ Parser.prototype = {
       else tag.text = new nodes.Text('\\n');
     }
 
-    tag.textOnly = ~textOnly.indexOf(tag.name);
+    tag.textOnly = tag.textOnly || ~textOnly.indexOf(tag.name);
 
     // script special-case
     if ('script' == tag.name) {

commit 2acb9e21efa895686052ec2d125aefd00ff6762d

only treat as pipeless textblock if there is additonal text after .
this issue was introduced in 5a62e630774e6639824068db3ad889517737f4bc
while this does support <elm> . <space> this is not really reasonable
for people whose editors are set to trim trailing whitespace as many are

lib/parser.js
@@ -654,7 +654,7 @@ Parser.prototype = {
       }
 
     // check immediate '.'
-    if ('.' == this.peek().val) {
+    if ('.' == this.peek().val && this.lookahead(2).val !== undefined) {
       dot = tag.textOnly = true;
       this.advance();
     }

test/cases/single-period.html
@@ -1,0 +1,1 @@
+<span>.</span>

test/cases/single-period.jade
@@ -1,0 +1,1 @@
+span .