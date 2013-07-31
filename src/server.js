bc3e002ad1d4e2503e6242efa3286328589219cb
ForbesLindesay <forbes@lindesay.co.uk>
2013-07-31 17:38:28 +0100

    Begin writing reference documentation

--- src/server.js
+++ src/server.js
@@ -34,6 +34,7 @@ app.use('/public', express.static(__dirname + '/public'));
 app.get('/', jade('./pages/index.jade'));
 app.get('/tutorial', jade('./pages/tutorial.jade'));
 app.get('/api', jade('./pages/api.jade'));
+app.get('/reference', jade('./pages/reference.jade', {doctypes: require('jade/lib/doctypes')}));
 app.get('/command-line', jade('./pages/command-line.jade'));
 app.get('/client.js', browserify('./client/index.js'))
 
