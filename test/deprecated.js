ecee77e172fff9bac7a9d6d9cd2b162106df5a55
Forbes Lindesay <forbes.lindesay@red-gate.com>
2014-02-28 13:37:00 +0000

    Deprecate removeAttribute/getAttribute

--- test/deprecated.js
+++ test/deprecated.js
@@ -4,13 +4,16 @@ var assert = require('assert');
 var jade = require('../');
 
 describe('deprecated functions', function () {
-  function deprecate(name, fn) {
+  function deprecate(name, fn, regex) {
     it(name, function () {
+      var consoleError = console.error;
       var consoleWarn = console.warn;
       var log = '';
       console.warn = function (msg) { log += msg; };
+      console.error = function (msg) { log += msg; };
       fn();
-      assert(new RegExp(name + ' is deprecated and will be removed in v2.0.0').test(log));
+      assert((regex || new RegExp(name + ' is deprecated and will be removed in v2.0.0')).test(log));
+      console.error = consoleError;
       console.warn = consoleWarn;
     });
   }
@@ -30,4 +33,23 @@ describe('deprecated functions', function () {
     var block = new jade.nodes.Block(new jade.nodes.Code('var x = 10'));
     block.replace({});
   });
+  deprecate('attrs.removeAttribute', function () {
+    var tag = new jade.nodes.Tag('a');
+    tag.setAttribute('foo', 'value');
+    tag.removeAttribute('href');
+    assert(!tag.getAttribute('href'));
+    tag.setAttribute('href', 'value');
+    tag.removeAttribute('href');
+    assert(!tag.getAttribute('href'));
+  });
+  deprecate('attrs.getAttribute', function () {
+    var tag = new jade.nodes.Tag('a');
+    tag.setAttribute('href', 'value');
+    assert(tag.getAttribute('href') === 'value');
+  });
+  deprecate('jade.compile(str, {client: true})', function () {
+    var fn = jade.compile('div', {client: true});
+    var fn = Function('jade', fn.toString() + '\nreturn template;')(jade.runtime);
+    assert(fn() === '<div></div>');
+  }, /The `client` option is deprecated/);
 });
