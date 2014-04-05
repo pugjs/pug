b0fb23c7962a353e82f3bf2bc45a83892f2750ad
Forbes Lindesay <forbes@lindesay.co.uk>
2014-04-05 15:11:29 +0100

    Warn on blocks with names that are never used
    [closes #885]

--- test/deprecated.js
+++ test/deprecated.js
@@ -3,20 +3,26 @@
 var assert = require('assert');
 var jade = require('../');
 
-describe('deprecated functions', function () {
-  function deprecate(name, fn, regex) {
-    it(name, function () {
-      var consoleError = console.error;
-      var consoleWarn = console.warn;
-      var log = '';
-      console.warn = function (msg) { log += msg; };
-      console.error = function (msg) { log += msg; };
+function deprecate(name, fn, regex) {
+  it(name, function () {
+    var consoleError = console.error;
+    var consoleWarn = console.warn;
+    var log = '';
+    console.warn = function (msg) { log += msg; };
+    console.error = function (msg) { log += msg; };
+    try {
       fn();
       assert((regex || new RegExp(name + ' is deprecated and will be removed in v2.0.0')).test(log));
+    } catch (ex) {
       console.error = consoleError;
       console.warn = consoleWarn;
-    });
-  }
+      throw ex;
+    }
+    console.error = consoleError;
+    console.warn = consoleWarn;
+  });
+}
+describe('deprecated functions', function () {
   deprecate('tag.clone', function () {
     var tag = new jade.nodes.Tag();
     tag.clone();
@@ -52,4 +58,10 @@ describe('deprecated functions', function () {
     var fn = Function('jade', fn.toString() + '\nreturn template;')(jade.runtime);
     assert(fn() === '<div></div>');
   }, /The `client` option is deprecated/);
+});
+
+describe('warnings that will become errors', function () {
+  deprecate('block that is never actually used', function () {
+    jade.renderFile(__dirname + '/fixtures/invalid-block-in-extends.jade');
+  }, /Warning: Unexpected block "contents" on line .* of .*.* This warning will be an error in v2.0.0/);
 });
