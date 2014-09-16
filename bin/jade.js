18ca70e8ae6f39076bd575ad680b7b5e3668275e
Forbes Lindesay <forbes.lindesay@red-gate.com>
2014-09-16 16:54:49 +0100

    Add doctype option to the command line

--- bin/jade.js
+++ bin/jade.js
@@ -34,6 +34,7 @@ program
   .option('-D, --no-debug', 'compile without debugging (smaller functions)')
   .option('-w, --watch', 'watch files for changes and automatically re-render')
   .option('--name-after-file', 'Name the template after the last section of the file path (requires --client and overriden by --name)')
+  .option('--doctype <str>', 'Specify the doctype on the command line (useful if it is not specified by the template)')
 
 
 program.on('--help', function(){
@@ -91,6 +92,10 @@ options.watch = program.watch;
 // --name
 
 options.name = program.name;
+
+// --doctype
+
+options.doctype = program.doctype;
 
 // left-over args are file paths
 
