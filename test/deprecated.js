3c28452eccb74b5c30cb0fea2f85d23f206b28de
Forbes Lindesay <forbes.lindesay@red-gate.com>
2014-12-19 16:28:44 +0000

    Remove tests for methods that will have been removed

--- test/deprecated.js
+++ test/deprecated.js
@@ -24,38 +24,6 @@ function deprecate(name, fn, regex) {
     console.warn = consoleWarn;
   });
 }
-describe('deprecated functions', function () {
-  deprecate('tag.clone', function () {
-    var tag = new jade.nodes.Tag();
-    tag.clone();
-  });
-  deprecate('node.clone', function () {
-    var code = new jade.nodes.Code('var x = 10');
-    code.clone();
-  });
-  deprecate('block.clone', function () {
-    var block = new jade.nodes.Block(new jade.nodes.Code('var x = 10'));
-    block.clone();
-  });
-  deprecate('block.replace', function () {
-    var block = new jade.nodes.Block(new jade.nodes.Code('var x = 10'));
-    block.replace({});
-  });
-  deprecate('attrs.removeAttribute', function () {
-    var tag = new jade.nodes.Tag('a');
-    tag.setAttribute('foo', 'value');
-    tag.removeAttribute('href');
-    assert(!tag.getAttribute('href'));
-    tag.setAttribute('href', 'value');
-    tag.removeAttribute('href');
-    assert(!tag.getAttribute('href'));
-  });
-  deprecate('attrs.getAttribute', function () {
-    var tag = new jade.nodes.Tag('a');
-    tag.setAttribute('href', 'value');
-    assert(tag.getAttribute('href') === 'value');
-  });
-});
 
 describe('warnings that will become errors', function () {
   deprecate('block that is never actually used', function () {
