1eeb9e6ea37a638601cc5559c37bf86ea4749b13
Timothy Gu <timothygu99@gmail.com>
2015-11-01 00:55:52 -0700

    Remove handling of options.externalRuntime
    It's more intuitive to have the same option name for all functions, albeit
    with different defaults.

--- register.js
+++ register.js
@@ -2,7 +2,7 @@ var jade = require('./');
 var resolvedJade = require.resolve('./');
 
 function compileTemplate(module, filename) {
-  var template = jade.compileFileClient(filename, {externalRuntime: true});
+  var template = jade.compileFileClient(filename, {inlineRuntimeFunctions: false});
   var body = "var jade = require('" + resolvedJade + "').runtime;\n\n" +
              "module.exports = " + template + ";";
   module._compile(body, filename);
