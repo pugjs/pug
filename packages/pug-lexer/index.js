e29d65da9d69fa602003e1e520103b4afb77e135
Kyle <kylekatarnls@users.noreply.github.com>
2017-01-29 11:30:27 +0100

    Support blank lines using the `|` syntax (#2695)
    * Report pug-lexer #79
    
    * Report unit test for pug-lexer #79
    
    * Fix misstyping in snapshot

--- packages/pug-lexer/index.js
+++ packages/pug-lexer/index.js
@@ -537,7 +537,8 @@ Lexer.prototype = {
 
   text: function() {
     var tok = this.scan(/^(?:\| ?| )([^\n]+)/, 'text') ||
-      this.scan(/^\|?( )/, 'text');
+      this.scan(/^( )/, 'text') ||
+      this.scan(/^\|( ?)/, 'text');
     if (tok) {
       this.addText('text', tok.val);
       return true;
