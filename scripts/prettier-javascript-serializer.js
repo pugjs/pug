ff9af95872c50ad7af26b01f5ec6dfd8c2454304
Forbes Lindesay <forbes@lindesay.co.uk>
2017-05-06 02:04:22 +0100

    Use prettier for JavaScript snapshots

---
+++ scripts/prettier-javascript-serializer.js
@@ -0,0 +1,21 @@
+const prettier = require('prettier');
+
+const prettierOptions = {} // optional
+
+// filename serializer that removes the basedir
+module.exports = {
+  test: function(val) {
+    try {
+      return (
+        typeof val === 'string' &&
+        /function /.test(val) &&
+        val !== prettier.format(val, prettierOptions)
+      );
+    } catch (ex) {
+      return false;
+    }
+  },
+  print: function(val, serialize, indent) {
+    return serialize(prettier.format(val, prettierOptions));
+  }
+};
