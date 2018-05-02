4b50db0365c79a8400a9d48b4ef105ba84c75f2f
Christopher Pappas <damassi.pappas@gmail.com>
2018-05-02 07:58:01 -0700

    [pug-filters] do not silence errors when attempting to require filters (#3002)

--- packages/pug-filters/lib/run-filter.js
+++ packages/pug-filters/lib/run-filter.js
@@ -8,36 +8,34 @@ var resolve = require('resolve');
 module.exports = filter;
 function filter(name, str, options, currentDirectory, funcName) {
   funcName = funcName || 'render';
-  var tr;
+  var trPath;
   try {
     try {
-      tr = require(resolve.sync('jstransformer-' + name, {basedir: currentDirectory || process.cwd()}));
+      trPath = resolve.sync('jstransformer-' + name, {basedir: currentDirectory || process.cwd()});
     } catch (ex) {
-      tr = require('jstransformer-' + name);
+      trPath = require.resolve('jstransformer-' + name);
     }
-    tr = jstransformer(tr);
-  } catch (ex) {}
-  if (tr) {
-    // TODO: we may want to add a way for people to separately specify "locals"
-    var result = tr[funcName](str, options, options).body;
-    if (options && options.minify) {
-      try {
-        switch (tr.outputFormat) {
-          case 'js':
-            result = uglify.minify(result, {fromString: true}).code;
-            break;
-          case 'css':
-            result = new CleanCSS().minify(result).styles;
-            break;
-        }
-      } catch (ex) {
-        // better to fail to minify than output nothing
-      }
-    }
-    return result;
-  } else {
+  } catch (ex) {
     var err = new Error('unknown filter ":' + name + '"');
     err.code = 'UNKNOWN_FILTER';
     throw err;
   }
+  var tr = jstransformer(require(trPath));
+  // TODO: we may want to add a way for people to separately specify "locals"
+  var result = tr[funcName](str, options, options).body;
+  if (options && options.minify) {
+    try {
+      switch (tr.outputFormat) {
+        case 'js':
+          result = uglify.minify(result, {fromString: true}).code;
+          break;
+        case 'css':
+          result = new CleanCSS().minify(result).styles;
+          break;
+      }
+    } catch (ex) {
+      // better to fail to minify than output nothing
+    }
+  }
+  return result;
 }
