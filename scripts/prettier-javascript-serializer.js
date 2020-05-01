c77448bc1c22b5bf3834f1a03c9179d2917d81da
Zack Schuster <email@zackschuster.com>
2020-05-01 10:50:39 -0700

    chore: use minimal settings to format test files

--- scripts/prettier-javascript-serializer.js
+++ scripts/prettier-javascript-serializer.js
@@ -1,16 +1,6 @@
-const fs = require('fs');
-const path = require('path');
 const prettier = require('prettier');
 
-const prettierrc = require('../.prettierrc.js');
-const prettierOptions = Object.assign(
-  {
-    trailingComma:
-      /** @type {import('prettier').ParserOptions['trailingComma']} */ ('es5'),
-  },
-  prettierrc,
-  prettierrc.overrides[0].options
-);
+const prettierOptions = {parser: 'babel'};
 
 // filename serializer that removes the basedir
 module.exports = {
