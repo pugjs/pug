017711cfe2701bd229883c5275de9e20e7908486
Alex <alex@brewingcode.net>
2020-05-06 09:09:54 -0700

    Appease prettier

--- scripts/buffer-serializer.js
+++ scripts/buffer-serializer.js
@@ -3,16 +3,16 @@ const crypto = require('crypto');
 // Buffer serializer to reduce snapshot gore for Node Buffer type
 module.exports = {
   test: function(val) {
-    return (
-      val &&
-      Buffer.isBuffer(val)
-    );
+    return val && Buffer.isBuffer(val);
   },
   print: function(val, serialize, indent) {
     const output = {
       type: 'Buffer',
       size: val.length,
-      hash: crypto.createHash('md5').update(val).digest('hex')
+      hash: crypto
+        .createHash('md5')
+        .update(val)
+        .digest('hex'),
     };
     return serialize(output);
   },
